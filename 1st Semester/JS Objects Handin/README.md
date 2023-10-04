bbab46d593dfb6fa2fc1bee5102850b30547bf70
Emily <kresky@protonmail.com>
2023-10-04 10:25:53 +0200

    Initial commit

---
+++ 1st Semester/JS Objects Handin/README.md
@@ -0,0 +1,3 @@
+## JavaScript Objects Handin
+Link to exercises:
+https://nicklasdean.gitbook.io/ita2023-1.-semester/web-technology/10-js-objects#handin-individual
---
+++ 1st Semester/JS Objects Handin/main.js
@@ -0,0 +1,50 @@
+/*
+--------------------------
+--- JavaScript Objects ---
+--------- HANDIN ---------
+--------------------------
+*/
+
+/*
+1) What to wear
+Create a function that has temperature as parameter.
+Based on temperature, it should return a string with what to wear.
+*/
+
+
+
+/*
+2) Dice game
+Create a function that simulators a dice roll.
+You call the function with the number of times you want to roll.
+Every time a 6 is rolled, log out "You just hit 6!"
+*/
+
+
+// Part 2 - If the user hits 6 in every roll, log out "Jackpot"
+
+
+/*
+3) Build a sentiment analyzer
+A sentiment analyzer is some functionality that figures out how positive/negative a sentence is.
+Eg. "I am mega super awesome happy" should have a high score.
+"I hate doing boring stuff" should have a low score.
+Create a function that takes a string as parameter.
+Calling the function will return an object with score, positiveWords and negativeWords.
+*/
+
+
+/*
+4) Character frequencies
+Write a function that counts the frequency of characters in a string.
+*/
+
+
+/*
+5) Credit Card number formatter
+Create a function that takes a number as parameter.
+The function should return the same number, but with a space after every 4 characters.
+It should be 16 characters long (so 4 groups of 4 numbers).
+To consider:
+    What happens if the function is called with an argument that is not a number?
+*/
