70feb796b9d31604340adb4c10bd2c160344a689
Kresky <kresky@protonmail.com>
2023-10-24 14:47:03 +0200

    Update parrot patter - Updated css styling - Upgrade buttons show once certain seed count has been met

--- 1st-Semester/Portfolio/portfolio-3-webtech/parrot-patter/main.js
+++ 1st-Semester/Portfolio/portfolio-3-webtech/parrot-patter/main.js
@@ -1,18 +1,32 @@
 /*
- * Cookie Clicker inspired game
+ * Main Parrot Clicker functionality
  */
 
+// Get head pat counter
 const patCounterElement = document.querySelector("#pat-counter");
 let patCounter = Number(patCounterElement.textContent);
 
+// Get seed counter
 const seedCounterElement = document.querySelector("#seed-counter");
 let seedCounter = Number(seedCounterElement.textContent);
 
+// Parrot clicking
 const calculateButton = document.querySelector("#parrot-button");
 calculateButton.addEventListener("click", function() {
+    // Add to seed counter
     seedCounter = seedCounter + upgradeHandCounter;
     seedCounterElement.textContent = seedCounter;
 
+    // Add to head pat counter
     patCounter = patCounter + upgradeHandCounter;
     patCounterElement.textContent = patCounter;
+
+    // Check if upgrade menu items should be displayed
+    if (seedCounter >= 10) {
+        document.querySelector("#upgrade-menu h2").style.display = "inline-block";
+        document.querySelector("#upgrade-hands").style.display = "inline-block";
+    }
+    if (seedCounter >= 85) {
+        document.querySelector("#upgrade-autopat").style.display = "inline-block";
+    }
 });
--- 1st-Semester/Portfolio/portfolio-3-webtech/parrot-patter/upgradeMenu.js
+++ 1st-Semester/Portfolio/portfolio-3-webtech/parrot-patter/upgradeMenu.js
@@ -1,6 +1,7 @@
 /*
  * Improved Hands Upgrade
  */
+// Get all needed elements
 const upgradeHandButton = document.querySelector("#upgrade-hands");
 const upgradeHandCounterElement = document.querySelector("#upgrade-hands-amount");
 let upgradeHandCounter = Number(upgradeHandCounterElement.textContent);
@@ -8,24 +9,48 @@ const upgradeHandPriceElement = document.querySelector("#upgrade-hands-price");
 let upgradeHandPrice = Number(upgradeHandPriceElement.textContent);
 upgradeHandButton.addEventListener("click", function() {
     if (seedCounter >= upgradeHandPrice) {
+        // Style button on click
+        upgradeHandButton.style.transform = "scale(1.0)";
+        upgradeHandButton.style.transition = "transform 0.1s ease-in-out";
+        upgradeHandButton.style.backgroundColor = "var(--color-accent)";
+
+        // Subtract price from seed counter
         seedCounter = seedCounter - upgradeHandPrice;
         seedCounterElement.textContent = seedCounter;
 
+        // Update hand counter
         upgradeHandCounter = upgradeHandCounter + 1;
         upgradeHandCounterElement.textContent = upgradeHandCounter;
 
+        // Update price, rounded down to whole number
         upgradeHandPrice = Math.floor(upgradeHandPrice * 2.25)
         upgradeHandPriceElement.textContent = upgradeHandPrice;
-
     }
 });
+
+// Style hand button on hover
 upgradeHandButton.addEventListener("mouseover", function() {
-    document.querySelector("#upgrade-button-mouseover-hands").style.display = "block"
+    document.querySelector("#upgrade-button-mouseover").textContent = "Increases the amount of pats you get per click"
+    if (seedCounter >= upgradeHandPrice) {
+        upgradeHandButton.style.backgroundColor = "var(--color-button-enabled)";
+        upgradeHandButton.style.transform = "scale(1.05)";
+        upgradeHandButton.style.cursor = "pointer";
+    } else {
+        upgradeHandButton.style.backgroundColor = "var(--color-button-disabled)";
+        upgradeHandButton.style.cursor = "not-allowed";
+    }
+});
+upgradeHandButton.addEventListener("mouseout", function() {
+    document.querySelector("#upgrade-button-mouseover").textContent = ""
+    upgradeHandButton.style.backgroundColor = "var(--color-accent)";
+    upgradeHandButton.style.transform = "scale(1.0)";
+    upgradeHandButton.style.cursor = "default";
 });
 
 /*
  * Automatic Patter Upgrade
  */
+// Get all needed elements
 const upgradeAutopatButton = document.querySelector("#upgrade-autopat");
 const upgradeAutopatCounterElement = document.querySelector("#upgrade-autopat-amount");
 let upgradeAutopatCounter = Number(upgradeAutopatCounterElement.textContent);
@@ -33,22 +58,51 @@ const upgradeAutopatPriceElement = document.querySelector("#upgrade-autopat-pric
 let upgradeAutopatPrice = Number(upgradeAutopatPriceElement.textContent);
 upgradeAutopatButton.addEventListener("click", function() {
     if (seedCounter >= upgradeAutopatPrice) {
+        // Style button on click
+        upgradeAutopatButton.style.transform = "scale(1.0)";
+        upgradeAutopatButton.style.transition = "transform 0.1s ease-in-out";
+        upgradeAutopatButton.style.backgroundColor = "var(--color-accent)";
+
+        // Subtract price from seed counter
         seedCounter = seedCounter - upgradeAutopatPrice;
         seedCounterElement.textContent = seedCounter;
 
+        // Update autopat counter
         upgradeAutopatCounter = upgradeAutopatCounter + 1;
         upgradeAutopatCounterElement.textContent = upgradeAutopatCounter;
 
+        // Update price, rounded down to whole number
         upgradeAutopatPrice = Math.floor(upgradeAutopatPrice * 1.35);
         upgradeAutopatPriceElement.textContent = upgradeAutopatPrice;
-    }
-    if (upgradeAutopatCounter === 1) {
-        setInterval(function() {
-            seedCounter = seedCounter + upgradeAutopatCounter;
-            seedCounterElement.textContent = seedCounter;
 
-            patCounter = patCounter + upgradeAutopatCounter;
-            patCounterElement.textContent = patCounter;
-        }, 1000)
+        // Start autopatting when first upgrade is bought
+        if (upgradeAutopatCounter === 1) {
+            setInterval(function() {
+                seedCounter = seedCounter + upgradeAutopatCounter;
+                seedCounterElement.textContent = seedCounter;
+
+                patCounter = patCounter + upgradeAutopatCounter;
+                patCounterElement.textContent = patCounter;
+            }, 1000)
+        }
     }
 })
+
+// Style autopat button on hover
+upgradeAutopatButton.addEventListener("mouseover", function() {
+    document.querySelector("#upgrade-button-mouseover").textContent = "Automatically pats the parrot every second"
+    if (seedCounter >= upgradeAutopatPrice) {
+        upgradeAutopatButton.style.backgroundColor = "var(--color-button-enabled)";
+        upgradeAutopatButton.style.transform = "scale(1.05)";
+        upgradeAutopatButton.style.cursor = "pointer";
+    } else {
+        upgradeAutopatButton.style.backgroundColor = "var(--color-button-disabled)";
+        upgradeAutopatButton.style.cursor = "not-allowed";
+    }
+});
+upgradeAutopatButton.addEventListener("mouseout", function() {
+    document.querySelector("#upgrade-button-mouseover").textContent = ""
+    upgradeAutopatButton.style.backgroundColor = "var(--color-accent)";
+    upgradeAutopatButton.style.transform = "scale(1.0)";
+    upgradeAutopatButton.style.cursor = "default";
+});
